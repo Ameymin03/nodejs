1a8924049164871a2c4efccf9b2198b736e29db2
Ameymin03 <152912235+Ameymin03@users.noreply.github.com>
2024-03-06 19:43:47 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('Amey');
+  res.end('Server running on port 4000');
+});
+server.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
