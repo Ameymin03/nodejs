0d4e6eb66b8915e67333fa995454f3305960f162
Ameymin03 <152912235+Ameymin03@users.noreply.github.com>
2024-03-11 17:44:38 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,9 +1,27 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log('Amey');
-  res.end('Server running on port 4000');
+    // Parse the requested URL
+    const url = req.url;
+
+    // Routing based on the requested URL
+    if (url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome home');
+    } else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to About Us page');
+    } else if (url === '/node') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to my Node Js project write a code');
+    } else {
+        // Handle other URLs
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
 });
-server.listen(4000, () => {
-  console.log('Server running on port 4000');
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
